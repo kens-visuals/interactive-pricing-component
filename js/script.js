047a4f9b46bc1b2e2fc775a3953009747562a6c8
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-03 12:30:08 -0800

    refactor JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let isChecked = false;
 
+const btn = document.querySelector('.js-btn');
 const num = document.querySelector('.js-num');
 const view = document.querySelector('.js-views');
 const slider = document.querySelector('.js-slider');
@@ -8,35 +9,59 @@ const checkbox = document.querySelector('.js-checkbox');
 const numsArr = ['8', '12', '16', '24', '36'];
 const viewsArr = ['10K', '50K', '100K', '500k', '1M'];
 
-checkbox.addEventListener('click', (e) => {
-  if (e.target.checked) {
-    const number = +num.textContent.slice(1);
+// Helpers
+const countDiscount = (num) => num - num * 0.25;
+const validateChecked = (num) =>
+  isChecked ? `$${countDiscount(num)}.00` : `$${num}.00`;
 
+// Callback functions
+const applyDiscount = function (e) {
+  const { checked } = e.target;
+  const number = +num.textContent.slice(1);
+
+  if (checked) {
     isChecked = true;
-    num.textContent = `$${number - number * 0.25}.00`;
+    num.textContent = `$${countDiscount(number)}.00`;
   } else {
     isChecked = false;
     num.textContent = `$${numsArr[slider.value - 1]}.00`;
   }
-});
+};
 
-console.log(isChecked);
-
-slider.addEventListener('input', (e) => {
+const setSliderBg = function (e) {
   const { value, min, max } = e.target;
-  const percentage = ((value - min) / (max - min)) * 100;
+  const bgPercentage = ((value - min) / (max - min)) * 100;
   const color = `linear-gradient(90deg, 
-    hsl(174, 76%, 80%) ${percentage}%, 
-    hsl(226, 68%, 95%) ${percentage}%)`;
+  hsl(174, 76%, 80%) ${bgPercentage}%, 
+  hsl(226, 68%, 95%) ${bgPercentage}%)`;
 
   slider.style.background = color;
-  num.textContent = isChecked
-    ? `$${numsArr[value - 1] - numsArr[value - 1] * 0.25}.00`
-    : `$${numsArr[value - 1]}.00`;
-  view.textContent = `${viewsArr[value - 1]} pageviews`;
-});
+};
 
-window.onload = function () {
+const setNumbers = function (e) {
+  const { value } = e.target;
+  const currNumValue = numsArr[value - 1];
+  const currViewValue = viewsArr[value - 1];
+
+  num.textContent = validateChecked(currNumValue);
+  view.textContent = `${currViewValue} pageviews`;
+};
+
+const reset = function () {
+  const color = `linear-gradient(90deg, 
+        hsl(174, 76%, 80%) 50%, 
+        hsl(226, 68%, 95%) 50%)`;
+
   slider.value = 3;
   checkbox.checked = false;
+  slider.style.background = color;
 };
+
+// Events
+slider.addEventListener('input', (e) => {
+  setNumbers(e);
+  setSliderBg(e);
+});
+checkbox.addEventListener('click', applyDiscount);
+btn.addEventListener('click', reset);
+window.addEventListener('load', reset);
